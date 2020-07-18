3d79fcceafcab22e7b523f8729a93a09ce6e4767
zzzzeu <zouanei@outlook.com>
2020-07-18 18:25:18 +0800

    week 9 homeworks

---
+++ Week_09/191.Everse words in a string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.strip()
+        i = j = len(s) - 1
+        res = []
+        while i >= 0:
+            while i >= 0 and s[i] != ' ': 
+                i -= 1
+            res.append(s[i + 1: j + 1])
+            while s[i] == ' ': 
+                i -= 1
+            j = i 
+        return ' '.join(res)
